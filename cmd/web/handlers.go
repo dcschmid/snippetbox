8f8964bc9c740641bd8e7250f9204bf33ee4e57c
Daniel Schmid <daniel.schmid@libertymail.net>
2021-04-25 21:13:10 +0200

    feat: single record sql queries

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"danschmid.de/snippetbox/pkg/models"
 )
 
 // Define a home handler function
@@ -60,9 +63,23 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    // Use the fmt.Fprintf() function to interpolate the id value with our response
-    // and write it to the http.ResponseWriter.
-    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+    // Use the SnippetModel object's Get method to retrieve the data for a
+    // specific record based on its ID. If no matching record is found,
+    // return a 404 Not Found response.
+    s, err := app.snippets.Get(id)
+
+    if err != nil {
+        if errors.Is(err, models.ErrNoRecord) {
+            app.notFound(w)
+        } else {
+            app.serverError(w, err)
+        }
+
+        return
+    }
+
+    // Write the snippet data as a plain-text HTTP response body.
+    fmt.Fprintf(w, "%v", s)
 }
 
 // Add a createSnippet handler function
