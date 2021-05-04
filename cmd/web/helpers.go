60e757b48e7a2a7189fcfe2688a78da2d3b52e4d
Daniel Schmid <daniel.schmid@libertymail.net>
2021-05-04 18:58:51 +0200

    feat: add csrf protection

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"runtime/debug"
 	"time"
+
+	"github.com/justinas/nosurf"
 )
 
 // The serverError helper writes an error message and stack trace to the errorLog,
@@ -40,6 +42,9 @@ func (app *application) addDefaultData(td *templateData, r *http.Request) *templ
     if td == nil {
         td = &templateData{}
     }
+
+    // Add the CSRF token to the templateData struct.
+    td.CSRFToken = nosurf.Token(r)
 
     td.CurrentYear = time.Now().Year()
     td.Flash = app.session.PopString(r, "flash")
